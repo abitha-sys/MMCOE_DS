0c26cb2e6958f8fa72dbfbf9b63ae04af6eabc3c
abitha-sys <abithanadar1111@gmail.com>
2025-11-05 10:57:33 +0530

    Refactor infix to postfix conversion using class

--- DS_PR_5(2).cpp
+++ DS_PR_5(2).cpp
@@ -1,75 +1,110 @@
-#include <stdio.h>
-#include <ctype.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+#define N 10
+using namespace std;
+class stack
+{
+	public:
+ char arr[10];
+ string ex;
+ int top;
+	stack()
+	{
+    	top=-1;
+	}
+	void push(char c)
+	{
+    	if(top==(N-1))
+    	{cout<<"stack overflow";}
+    	else{top+=1;
+    	arr[top]=c;}
+	}
+	char pop()
+	{   char c= ' ';
+    	if(top==-1)
+    	{//cout<<"Stack underflow";
+    	}
+    	else
+    	{
+        	c=arr[top];
+        	top --;
+    	}
+    	return c;
+	}
+	int precedence(char opr){
+    	if (opr=='*' || opr=='/')
+        	return 2;
+    	if (opr=='+' || opr=='-')
+        	return 1;
+   	if(opr=='(')  return 0;
+   	    return -1;  // Default for unknown characters
 
-#define SIZE 50
+	}
+	char associativity(char opr){
+    	if (opr=='*' || opr=='/'||opr=='+' || opr=='-')
+        	return 'L';
+    	else
+        	return 'R';
+	}
+	char peek(){return arr[top]; }
+	string InfixToPostfixConversion(string ex);
+};
 
-char stack[SIZE];
-int top = -1;
+string stack::InfixToPostfixConversion(string ex){
+	int l=ex.length();
+	int i=0,j=0;
+	char op_exp[20];
+	char ch,ch1;
+	while(i<l){
 
-// push function
-void push(char ch) {
-    top++;
-    stack[top] = ch;
-}
-
-// pop function
-char pop() {
-    char ch = stack[top];
-    top--;
-    return ch;
-}
-
-// function to set operator priority
-int precedence(char ch) {
-    if (ch == '+' || ch == '-')
-        return 1;
-    if (ch == '*' || ch == '/')
-        return 2;
-    return 0;
-}
-
-int main() {
-    char infix[SIZE], postfix[SIZE];
-    int i = 0, j = 0;
-    char ch;
-
-    printf("Enter infix expression: ");
-    scanf("%s", infix);
-
-    while ((ch = infix[i]) != '\0') {
-        // if operand, add to postfix
-        if (isalnum(ch)) {
-            postfix[j++] = ch;
-        }
-        // if '(', push it
-        else if (ch == '(') {
-            push(ch);
-        }
-        // if ')', pop until '('
-        else if (ch == ')') {
-            while (stack[top] != '(') {
-                postfix[j++] = pop();
-            }
-            pop(); // remove '('
-        }
-        // if operator
-        else {
-            while (top != -1 && precedence(stack[top]) >= precedence(ch)) {
-                postfix[j++] = pop();
-            }
-            push(ch);
-        }
-        i++;
-    }
-
-    // pop remaining operators
-    while (top != -1) {
-        postfix[j++] = pop();
-    }
-
-    postfix[j] = '\0'; // end string
-
-    printf("Postfix expression: %s\n", postfix);
-
-    return 0;
-}
+     	//cout<<ex[i];  
+     	if(ex[i]=='+' ||ex[i]=='-'||ex[i]=='*' || ex[i]=='/'){
+       	 
+        	if(top==-1) push(ex[i]);
+        	else{
+             	ch=peek();
+             	//if(ch== ' ')
+             	while(precedence(ex[i])<=precedence(ch)){
+               	ch1=pop();
+               	op_exp[j++]=ch1;
+               	ch=peek();
+            	}
+            	push(ex[i]);
+        	}
+     	}
+    	else if(ex[i]=='(')
+        	push(ex[i]);
+    	else if(ex[i]==')'){
+        	ch1=pop();
+        	while(ch1!='('){
+            	op_exp[j++]=ch1;
+            	ch1=pop();
+        	}
+    	}
+    	else  {
+        	op_exp[j++]=ex[i];
+       	// var++;
+    	}
+    	i++;
+   	 
+	}
+	do{
+    	ch=pop();
+    	op_exp[j++]=ch;
+	}while(ch!=' ');
+	op_exp[j]='\0';
+	return op_exp;
+    
+	}
+ 
+ int main() {  
+  stack s;
+  string ex;
+  cout<<"\n Enter an expression:";
+  cin>>ex;
+  string op_exp=s.InfixToPostfixConversion(ex);
+  cout<<"\n Postfix Expression is: "<<op_exp;
+  return 0;
+	 
+   }
