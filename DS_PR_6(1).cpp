c752befb7b7205c6254c63bf19e2ec36bff989ae
abitha-sys <abithanadar1111@gmail.com>
2025-11-11 12:48:25 +0530

    Refactor Coffee_shop class methods and menu

--- DS_PR_6(1).cpp
+++ DS_PR_6(1).cpp
@@ -11,13 +11,8 @@ Coffee_shop () {
 rear = -1;
 front = -1;
 }
-bool isFull();
-bool isEmpty();
-void enQueue(int t);
-int deQueue();
-};
 
-bool Coffee_shop::isFull () {
+bool isFull () {
 if (rear == MAXSIZE-1) {
 return 1;
 } else {
@@ -25,7 +20,7 @@ return 0;
 }
 }
 
-bool Coffee_shop::isEmpty () {
+bool isEmpty () {
 if (front == -1 || front == MAXSIZE || rear < front) {
 return 1;
 } else {
@@ -33,7 +28,7 @@ return 0;
 }
 }
 
-void Coffee_shop::enQueue (int t) {
+void enQueue (int t) {
 if (isFull()) {
 cout << "Sorry! The order can't be placed\nThe queue is full!" << endl;
 } else {
@@ -42,35 +37,36 @@ front = 0;
 }
 rear = rear + 1;
 token[rear] = t;
+cout<<"order "<<token[rear]<<" is added to the queue";
 }
 }
 
-int Coffee_shop::deQueue () {
+int deQueue () {
 if (isEmpty()) {
 cout << "The queue is empty." << endl;
 return 0;
 } else {
 int t = token[front];
-cout <<	 "Order processed for: " << token[front] << endl;
+cout <<	 "Order processed for: " << token[front] <<" and served"<< endl;
 front += 1;
 return t;
 }
 }
-
+};
 int main() {
 Coffee_shop customer;
 int choice = 0, order;
 do {
 cout << "Coffee shop menu:" << endl;
-cout << " 1. Add an order\n 2. Delete an order (received)\n 3. Exit\nChoice: ";
+cout << " 1. Add an order\n 2. Serve order\n 3. Exit\nChoice: ";
 	cin >> choice;
 switch (choice) {
-case 1: cout << "\nEnter value to be inserted as order: ";
+case 1: cout << "\nEnter your order number: ";
 cin >> order;
 customer.enQueue(order);
 cout << endl;
 break;
-case 2:	cout << "\nDeleting:" << endl;
+case 2:	cout << "\nprocessing the order:" << endl;
 customer.deQueue();
 cout << endl;
 break;
